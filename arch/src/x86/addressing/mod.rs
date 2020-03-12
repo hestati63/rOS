ac656c5f82d3c260b3120c81fc8803a2ef5dd86b
Minkyu Jung <hestati@kaist.ac.kr>
2020-03-12 16:42:52 +0900

    arch: Add macro for page

--- arch/src/x86/addressing/mod.rs
+++ arch/src/x86/addressing/mod.rs
@@ -29,4 +29,37 @@ impl Virtual {
     pub const fn to_u64(&self) -> u64 {
         self.0
     }
+
+    pub unsafe fn as_mut<T>(&self) -> Option<&'static mut T> {
+        (self.to_u64() as *mut T).as_mut()
+    }
+
+    pub unsafe fn as_ref<T>(&self) -> Option<&'static T> {
+        (self.to_u64() as *mut T).as_ref()
+    }
+}
+
+pub const PG_SHIFT: u64 = 12;
+pub const PAGE_SIZE: u64 = 1 << PG_SHIFT;
+pub const PAGE_MASK: u64 = PAGE_SIZE - 1;
+
+#[macro_export]
+macro_rules! page_up {
+    ($arg: expr) => {
+        ($arg + $crate::PAGE_MASK) & !$crate::PAGE_MASK
+    };
+}
+
+#[macro_export]
+macro_rules! page_down {
+    ($arg: expr) => {
+        $arg & !$crate::PAGE_MASK
+    };
+}
+
+#[macro_export]
+macro_rules! pfn {
+    ($arg: expr) => {
+        $arg >> $crate::PG_SHIFT
+    };
 }
